c10d56b89da807249a7f1b3039ae481ae6950b77
HEITOR DEV <90859842+Heitordesv@users.noreply.github.com>
2026-08-22 00:54:02 -0300

    feat: vincula sangria à abertura do caixa

--- app/Models/SangriaCaixa.php
+++ app/Models/SangriaCaixa.php
@@ -7,10 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class SangriaCaixa extends Model
 {
     protected $fillable = [
-        'usuario_id', 'valor', 'empresa_id', 'observacao'
+        'usuario_id', 'valor', 'empresa_id', 'observacao', 'abertura_caixa_id'
     ];
 
     public function usuario(){
         return $this->belongsTo(Usuario::class, 'usuario_id');
+    }
+
+    public function aberturaCaixa()
+    {
+        return $this->belongsTo(AberturaCaixa::class, 'abertura_caixa_id');
     }
 }
